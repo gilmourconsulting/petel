e78a3005a4afd244d73988516a98f265a99e4062
Asher Gilmour <asher@gilmour.info>
2026-01-14 13:45:37 +0200

    Schoollist screen added

---
+++ PetelApp.BlazorServer/DTOs/SchoolDto.cs
@@ -0,0 +1,16 @@
+namespace PetelApp.BlazorServer.DTOs
+{
+    public class SchoolDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Symbol { get; set; }
+        public string? Address { get; set; }
+        public string? PrincipalName { get; set; }
+        public string? InspectorName { get; set; }
+        public string? CharacterizationName { get; set; }
+        public string? ContactPerson { get; set; }
+        public string? EducationStage { get; set; }
+        public string? OwnerName { get; set; }
+    }
+}
